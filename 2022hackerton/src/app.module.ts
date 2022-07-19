14a2aa66cc132c27175a182c6b6ba8c2a846e126
김준호 <kimjunho@gimjunhcBookPro.lan>
2022-07-19 11:53:29 +0900

    fix error of database module

--- 2022hackerton/src/app.module.ts
+++ 2022hackerton/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './config/database/database.module';
 import { TokenModule } from './token/token.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, TokenModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TokenModule,
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
