8de1172d24ff25b348f3991e63b7f7136083b4c3
김준호 <kimjunho@gimjunhcBookPro.lan>
2022-07-19 12:08:25 +0900

    add loggin interceptor

--- 2022hackerton/src/auth/auth.controller.ts
+++ 2022hackerton/src/auth/auth.controller.ts
@@ -2,13 +2,15 @@ import { Body, Controller, Post } from '@nestjs/common';
 import BaseResponse from 'src/global/response/baseResponse';
 import { AuthService } from './auth.service';
 import DauthLoginDto from './dto/dauth.login.dto';
+import { IloginData } from './interface/IloginData';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  async getCodeLogin(@Body() dauthLoginDto: DauthLoginDto) {
+  async getCodeLogin(@Body() dto: DauthLoginDto) {
+    const data: IloginData = await this.authService.login(dto);
     return BaseResponse.successResponse('Dauth 로그인 성공');
   }
 }
